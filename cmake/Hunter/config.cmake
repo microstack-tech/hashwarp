d4bb85933f7704e5dc4363f144d0b06608872bec
Julian Yap <julianokyap@gmail.com>
2024-10-17 20:00:48 -0700

    Pin CLI11 version 1.7.1

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,3 +1,4 @@
 hunter_config(CURL VERSION ${HUNTER_CURL_VERSION} CMAKE_ARGS HTTP_ONLY=ON CMAKE_USE_OPENSSL=OFF CMAKE_USE_LIBSSH2=OFF CURL_CA_PATH=none)
 hunter_config(Boost VERSION 1.66.0)
+hunter_config(CLI11 VERSION 1.7.1)
 hunter_config(ethash GIT_SUBMODULE "cmake/ethash")
